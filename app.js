27815d3c5c16dfd7c6d73e8e0008f2978c0cfa94
j-burlison <jburliso@redhat.com>
2018-01-29 10:30:04 -0600

    Simplify routes and add OSE Route

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var proxy = require('http-proxy-middleware');
 
 var app = express();
 
-app.use('/api/google', proxy({target: 'https://www.google.com', changeOrigin: true}));
+app.use('/', proxy({target: 'https://www.google.com', changeOrigin: true}));
 
-app.use('/api/proxy', proxy({target: 'https://www.google.com', changeOrigin: true}));
+app.use('/proxy', proxy({target: 'http://node-service-josh-test.apps.employers.rht-labs.com', changeOrigin: true}));
 
 app.listen(3000);
