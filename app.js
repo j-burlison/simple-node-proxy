9c0a9d51b70b8112ef88cea41e78a6d1ccdd430f
j-burlison <jburliso@redhat.com>
2018-01-29 10:25:20 -0600

    Start script

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ var proxy = require('http-proxy-middleware');
 
 var app = express();
 
-app.use('/', proxy({target: 'https://www.google.com', changeOrigin: true}));
+app.use('/api/google', proxy({target: 'https://www.google.com', changeOrigin: true}));
+
+app.use('/api/proxy', proxy({target: 'https://www.google.com', changeOrigin: true}));
+
 app.listen(3000);
