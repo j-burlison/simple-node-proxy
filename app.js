8f52600268eedc08eb13f491c2db7704ea58bf2d
j-burlison <jburliso@redhat.com>
2018-01-29 10:39:04 -0600

    Set up Proxy Route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var proxy = require('http-proxy-middleware');
 
 var app = express();
 
-app.use('/', proxy({target: 'https://www.google.com', changeOrigin: true}));
+app.use('/google', proxy({target: 'https://www.google.com', changeOrigin: true}));
 
 app.use('/proxy', proxy({target: 'http://node-service-josh-test.apps.employers.rht-labs.com', changeOrigin: true}));
 
