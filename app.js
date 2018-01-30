fb396823204e4d88c27ba28417a172ed580b755a
j-burlison <jburliso@redhat.com>
2018-01-29 18:25:19 -0600

    WORKING

--- app.js
+++ app.js
@@ -5,23 +5,7 @@ var proxy = require('http-proxy-middleware');
 
 app.use(cors());
 
-// app.use('/client', proxy({target: 'http://0.0.0.0:8090/api', changeOrigin: false}));
 app.use('/client', proxy({target: 'http://node-service-josh-test.apps.employers.rht-labs.com/api', changeOrigin: true}));
-
-var request = require('request');
-
-// app.use('/client', (req, res, next)=>{
-//   console.log('PROXY HIT: '+'http://node-service-josh-test.apps.employers.rht-labs.com/api/client'+req.url);
-//
-//   request('http://node-service-josh-test.apps.employers.rht-labs.com/api/client'+req.url, (err, response)=>{
-//     console.log('PROXY GOT A RESPONSE');
-//     res.send(response.body);
-//   });
-//   // request('http://0.0.0.0:8090/api/client'+req.url, (err, response)=>{
-//   //   console.log('PROXY GOT A RESPONSE: ');
-//   //   res.send(response.body);
-//   // });
-// });
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
