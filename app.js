563a7a64cd151428cce8ec65f4e3dd44b1714680
j-burlison <jburliso@redhat.com>
2018-01-29 22:04:37 -0600

    CORS for Dev Only

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ var app = express();
 var cors = require('cors');
 var proxy = require('http-proxy-middleware');
 
-app.use(cors());
-
-app.use('/client', proxy({target: 'http://node-service-josh-test.apps.employers.rht-labs.com/api', changeOrigin: true}));
-
+// Env Vars can be set in OSE
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+// Enable CORS. For local Development and De-coupled UI Deployment and Code Base
+if (process.env.NAME === 'DEV') {
+  app.use(cors());
+}
 
+// Route all '/client' REST traffic to an OSE Service that has exposed endpoints at '/api/client'
+app.use('/client', proxy({target: 'http://node-service-josh-test.apps.employers.rht-labs.com/api', changeOrigin: true}));
+
+// Serve Static Files to '/index' (for UI)
 app.use('/index', express.static(__dirname));
 
+// Start App
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
-module.exports = app ;
+// Expose App Routes
+module.exports = app;
